59e4b6d09f74f55f1ef1c7358f72d3ba92e18383
Maya <mayaibragimova04@gmail.com>
2024-07-17 09:42:04 +0300

    write a home work

---
+++ skript.js
@@ -0,0 +1,38 @@
+let firstUserName = "Nooruz";
+let firstUserPoint = 97;
+console.log(`${firstUserName}     - ${firstUserPoint} баллов из 100 `);
+
+let secondUserName = "Zhakshylyk";
+let secondUserPoint = 89;
+console.log(`${secondUserName} - ${secondUserPoint} баллов из 100`);
+
+let thirdUserName = "Alisher";
+let thirdUserPoint = 75;
+console.log(`${thirdUserName}    - ${thirdUserPoint} баллов из 100 `);
+let fourthUserName = "Elizar";
+let fourthUserPoint = 90;
+console.log(`${fourthUserName}     - ${fourthUserPoint} баллов из 100`);
+let multiplicationTable = 4 
+console.log(`
+ ${1} x ${  multiplicationTable } =  ${multiplicationTable * 1}
+ ${2} x ${  multiplicationTable } =  ${multiplicationTable * 2}
+ ${3} x ${  multiplicationTable } =  ${multiplicationTable * 3} 
+ ${4} x ${  multiplicationTable } =  ${multiplicationTable * 4}
+ ${5} x ${  multiplicationTable } =  ${multiplicationTable * 5}
+ ${6} x ${  multiplicationTable } =  ${multiplicationTable * 6}
+ ${7} x ${  multiplicationTable } =  ${multiplicationTable * 7}
+ ${8} x ${  multiplicationTable } =  ${multiplicationTable * 8}
+ ${9} x ${  multiplicationTable } =  ${multiplicationTable * 9}
+ ${10} x ${multiplicationTable } = ${multiplicationTable * 10}
+ `);
+
+
+ let UserName = prompt('введите ваше имя');
+ console.log(UserName);
+ let registration = alert ('Вы успешно зарегистрировались!');
+ console.log(registration);
+ let questions= confirm('При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?');
+ console.log(questions);
+
+
+
